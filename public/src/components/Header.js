5dd84f5ddfcc48ccf55bc743dc71c0a112834d6c
evanandrewson <evanandrewson@gmail.com>
2019-08-27 13:49:54 -0700

    trying to fix Heroku errors

--- public/src/components/Header.js
+++ public/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     renderHTML() {
         return /*html*/`
         <header>
-            <img src="./public/assets/logo.png" alt="logo">
+            <img src="/public/assets/logo.png" alt="logo">
             <h1>To Do List</h1>
         </header>
         `;
