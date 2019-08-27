c2b725e28ff2663032d4c2d23543efe3629ada2c
evanandrewson <evanandrewson@gmail.com>
2019-08-27 13:46:21 -0700

    started creating test

---
+++ public/test/to-do-item.test.js
@@ -0,0 +1,33 @@
+import ToDoItem from '../src/components/ToDoItem.js';
+
+const test = QUnit.test;
+
+QUnit.module('render to do item');
+
+test('render to do item', assert => {
+    // arrange
+    const todo = {
+        id: 1,
+        item: 'do laundry',
+        completed: false
+    };
+    const expected = `
+    <li class="to-do">
+        <span class="''">do laundry</span>
+        <div>
+            <button class="inactive-button">
+                Make Inactive
+            </button>
+            <button class="remove-button">
+                🗑
+            </button>
+        </div>
+    </li>
+    `;
+    // act
+    const toDoItem = new ToDoItem({ todo });
+    const html = toDoItem.renderHTML();
+    
+    // assert
+    assert.htmlEqual(html, expected);
+});
